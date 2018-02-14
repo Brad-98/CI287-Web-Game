d2bc609a5f36a6fff3cb3bd9ecb6bd29f05945e5
Sam-Thomson <S.Thomson3@uni.birghton.ac.uk>
2018-02-14 18:21:10 +0000

    i

--- source/level1.js
+++ source/level1.js
@@ -1,8 +1,11 @@
-<<<<<<< HEAD
+
 function add(int x, int y)
 {
     return x + y;
 }
-=======
-g
->>>>>>> 38801d564ef62ffd241b596d734598b26aa2c0ac
+
+function remove(int x, int y)
+{
+    return x - y;
+}
+=
