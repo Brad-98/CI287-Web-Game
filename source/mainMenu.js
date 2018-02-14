6e58035ac8265849ed2862608e636faba6b59fec
Bradley Modern <modern.bradley@gmail.com>
2018-02-14 23:57:21 +0000

    Added in a tower sprite to the main menu + the png files

--- source/mainMenu.js
+++ source/mainMenu.js
@@ -1,17 +1,23 @@
 var GAMEHEIGHT = 720;
 var GAMEWIDTH = 1280;
 
+var towerSprite;
+
 var mainMenu = 
 {
     preload : function()
     {
         this.game.load.image('playButton', '../assets/playButton.png');
+        this.game.load.image('towerSprite', '../assets/towerSprite.png');
     },  
     
     create : function()
     {
         this.game.add.text(GAMEWIDTH/4.5, GAMEHEIGHT/12, 'Ascend The Tower!', { font: '80px Arial', fill: '#fff' });
         this.add.button(GAMEWIDTH/2.7, GAMEHEIGHT/3, 'playButton', this.gotoOutdoorZone, this);
+        
+        towerSprite = this.game.add.sprite(GAMEWIDTH/1.35, GAMEHEIGHT/4, 'towerSprite');
+        towerSprite.scale.setTo(3, 3);
     },
     
     gotoOutdoorZone : function()
