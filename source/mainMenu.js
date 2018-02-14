70c969769b4cf147f46219c68529a72c5a4c65a5
Bradley Modern <modern.bradley@gmail.com>
2018-02-14 22:55:34 +0000

    Added in a play button

--- source/mainMenu.js
+++ source/mainMenu.js
@@ -5,13 +5,13 @@ var mainMenu =
 {
     preload : function()
     {
-        //Preload the sprites here
+        this.game.load.image('playButton', '../assets/playButton.png');
     },  
     
     create : function()
     {
-        //Create buttons and text here
-        this.game.add.text(GAMEWIDTH/3.5, GAMEHEIGHT/3, 'Climb The Tower!', { font: '80px Arial', fill: '#fff' });
+        this.game.add.text(GAMEWIDTH/4.5, GAMEHEIGHT/12, 'Ascend The Tower!', { font: '80px Arial', fill: '#fff' });
+        this.add.button(GAMEWIDTH/2.7, GAMEHEIGHT/3, 'playButton', this.gotoOutdoorZone, this);
     },
     
     gotoOutdoorZone : function()
