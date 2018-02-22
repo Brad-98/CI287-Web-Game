69b1f7e7b9a2eaeee7d09c49141f84040ebca51b
Sam-Thomson <S.Thomson3@uni.birghton.ac.uk>
2018-02-22 01:55:08 +0000

    Fixing diagonal error

--- source/outdoorZone.js
+++ source/outdoorZone.js
@@ -1,8 +1,8 @@
 var player;
 var enemy_elf;
-var enemy_elf2;
-var enemy_elf3;
-var enemy_elf4;
+//var enemy_elf2;
+//var enemy_elf3;
+//var enemy_elf4;
 var controls;
 
 var outdoorZone =
@@ -11,6 +11,13 @@ var outdoorZone =
     {
         this.game.load.spritesheet('player', '../assets/player.png',64,65,77);
         this.game.load.spritesheet('enemy_elf', '../assets/enemy_elf.png',64,69,66);
+        
+        enemy_elf = function (index, game, x, y) {
+            this.enemy_elf = game.add.sprite(x,y,"enemy_elf");
+            this.enemy_elf.anchor.setTo(0.5,0.5);
+            this.enemy_elf.name = index.toString;
+            game.physics.enable(this.enemy_elf,Phaser.Physics.ARCADE);     
+};
     },  
     
     create : function()
@@ -32,25 +39,25 @@ var outdoorZone =
         
         //Enemy Elf Code
         
-        this.enemy_elf = game.add.sprite(100,180,"enemy_elf");
+        new enemy_elf(0,game,100,100);
+
         
-        this.enemy_elf.animations.add('walkUp', [0,1,2,3,4,5,6,7,8],12, true);
+        //this.enemy_elf = game.add.sprite(100,180,"enemy_elf");
         
-        this.enemy_elf.animations.add('walkDown', [18,19,20,21,22,23,24,25,26],12, true);
+        //this.enemy_elf.animations.add('walkUp', [0,1,2,3,4,5,6,7,8],12, true);
         
-        this.enemy_elf.animations.add('walkLeft', [27,28,29,30,31,32,33,34,35],12, true);
+        //this.enemy_elf.animations.add('walkDown', [18,19,20,21,22,23,24,25,26],12, true);
         
-        this.enemy_elf.animations.add('walkRight', [9,10,11,12,13,14,15,16,17],12, true);
+       // this.enemy_elf.animations.add('walkLeft', [9,10,11,12,13,14,15,16,17],12, true);
         
+       // this.enemy_elf.animations.add('walkRight', [27,28,29,30,31,32,33,34,35],12, true);
         
-        
-        
-        this.enemy_elf2 = game.add.sprite(160,180,"enemy_elf");
-        this.enemy_elf2.animations.add('walkDown', [18,19,20,21,22,23,24,25,26],12, true);
-        this.enemy_elf3 = game.add.sprite(220,180,"enemy_elf");
-        this.enemy_elf3.animations.add('walkLeft', [27,28,29,30,31,32,33,34,35],12, true);
-        this.enemy_elf4 = game.add.sprite(280,180,"enemy_elf");
-        this.enemy_elf4.animations.add('walkRight', [9,10,11,12,13,14,15,16,17],12, true);
+        //this.enemy_elf2 = game.add.sprite(160,180,"enemy_elf");
+        //this.enemy_elf2.animations.add('walkDown', [18,19,20,21,22,23,24,25,26],12, true);
+        //this.enemy_elf3 = game.add.sprite(220,180,"enemy_elf");
+        //this.enemy_elf3.animations.add('walkLeft', [27,28,29,30,31,32,33,34,35],12, true);
+        //this.enemy_elf4 = game.add.sprite(280,180,"enemy_elf");
+        //this.enemy_elf4.animations.add('walkRight', [9,10,11,12,13,14,15,16,17],12, true);
     
         //Player Movement controls
         controls = 
@@ -64,10 +71,10 @@ var outdoorZone =
     
     update : function()
     {
-        this.enemy_elf.animations.play('walkUp');
-        this.enemy_elf2.animations.play('walkDown');
-        this.enemy_elf3.animations.play('walkLeft');
-        this.enemy_elf4.animations.play('walkRight');
+        //this.enemy_elf.animations.play('walkUp');
+        //this.enemy_elf2.animations.play('walkDown');
+        //this.enemy_elf3.animations.play('walkLeft');
+        //this.enemy_elf4.animations.play('walkRight');
         
         if(controls.up.isDown)
         {
@@ -75,19 +82,19 @@ var outdoorZone =
             this.player.y -=2;
         }
         
-        if(controls.left.isDown)
+        else if(controls.left.isDown)
         {
             this.player.animations.play('walkLeft');
             this.player.x -=2;
         }
         
-        if(controls.down.isDown)
+        else if(controls.down.isDown)
         {
             this.player.animations.play('walkDown');
             this.player.y +=2;
         }
         
-        if(controls.right.isDown)
+        else if(controls.right.isDown)
         {
             this.player.animations.play('walkRight');
             this.player.x +=2;
