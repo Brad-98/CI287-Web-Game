1ed3810aa639ee199f3608f329742a5a157a4d7d
Sam-Thomson <S.Thomson3@uni.birghton.ac.uk>
2018-03-21 12:36:48 +0000

    Coin sprite

---
+++ assets/tilesets/Grass&Sand.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Grass&amp;Sand" tilewidth="32" tileheight="32" spacing="1" tilecount="25" columns="5">
+ <image source="Grass&amp;Sand.png" width="192" height="192"/>
+</tileset>
