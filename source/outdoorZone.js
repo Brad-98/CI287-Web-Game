2ff3aec6198efcd5956545838018d70724e021d6
Sam-Thomson <S.Thomson3@uni.birghton.ac.uk>
2018-04-13 00:42:48 +0100

    Merchant Death sprite

--- source/outdoorZone.js
+++ source/outdoorZone.js
@@ -79,6 +79,7 @@ var outdoorZone =
         this.game.load.spritesheet('fireball', '../assets/fireball.png',64,64,63);
         this.game.load.image('heart', '../assets/player_heart.png');
         this.game.load.image('heart_upgrade', '../assets/player_heartUpgrade.png');
+        this.game.load.image('arrow', '../assets/arrow.png');
         
         this.game.load.tilemap('map','../assets/tilesets/outdoorZone..json', null, Phaser.Tilemap.TILED_JSON);
         this.game.load.image('tileSheet', '../assets/tilesets/tileset_outdoor.png');
@@ -223,10 +224,7 @@ var outdoorZone =
     },
     
     update : function()
-    {  // this.game.debug.body(this.towerSprite);
-     //this.game.debug.body(enemy_up);
-     //this.game.debug.body(enemy_down);
-     //this.game.debug.body(enemy_left);
+    {  
         if(player.alive)
         {
             this.game.physics.arcade.collide(player, world_outdoorZone.layer_walls);
@@ -360,6 +358,7 @@ var outdoorZone =
                {
                    enemy_elf_up.callAll('play',null,'fireUp');
                    elfTween_up.pause();
+                   this.shootArrow();
                }
             else
                {
@@ -624,6 +623,11 @@ var outdoorZone =
         fireball.kill();
         enemy_right.kill();
     },
+    
+    shootArrow : function()
+    {
+        
+    },
       
     collectCoin : function(player,coin)
     {
@@ -640,5 +644,5 @@ var outdoorZone =
     gotoTowerLevel : function(player, towerSprite)
     {
         game.state.start('tower_level');
-    },       
+    }, 
 };
